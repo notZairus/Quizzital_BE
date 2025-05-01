44b1529b6832891812135ed14298a324d4f884d3
notZairus <notzairus@gmail.com>
2025-05-01 19:15:54 +0800

    added gunicorn

--- configs.py
+++ configs.py
@@ -14,7 +14,7 @@ CORS(app)
 
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
